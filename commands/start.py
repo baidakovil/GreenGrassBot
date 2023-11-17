3eab46408788457bcb9f258197f60b1a4ea342a8
baidakovil <baidakovil@gmail.com>
2023-11-17 23:58:20 +0300

    Docs, typing, snake_case, blackened, i18n

--- commands/start.py
+++ commands/start.py
@@ -1,16 +1,19 @@
 import i18n
+from telegram import Update
+from telegram.ext import CallbackContext
 
 from db.db import Db
 from services.message_service import reply
-from telegram import Update
-from telegram.ext import CallbackContext
+from services.message_service import alarm_char
 
 db = Db()
 
 
 async def start(update: Update, context: CallbackContext) -> None:
     """
-    Sends start message. If user have accounts, list it.
+    Callback function. Sends start message. If user have accounts, list it.
+    Args:
+        update, context: standart PTB callback signature
     """
     await db.save_user(update)
     user_id = update.message.from_user.id
@@ -19,11 +22,13 @@ async def start(update: Update, context: CallbackContext) -> None:
     if not lfm_accs:
         text = i18n.t('start.user', username=username)
     else:
-        lfm_accs = ['_' + acc + '_' for acc in lfm_accs]
-        text = i18n.t('start.hacker',
-                      username=username,
-                      qty=len(lfm_accs),
-                      accs=', '.join(lfm_accs),
-                      )
+        undscr = alarm_char('_', replace=True)
+        lfm_accs = [undscr + acc + undscr for acc in lfm_accs]
+        text = i18n.t(
+            'start.hacker',
+            username=username,
+            qty=len(lfm_accs),
+            accs=', '.join(lfm_accs),
+        )
     await reply(update, text)
     return None
