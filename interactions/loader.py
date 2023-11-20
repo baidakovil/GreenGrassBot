1c58ceb50b9e2c3b260430c5dc2bd98fb055ada3
baidakovil <baidakovil@gmail.com>
2023-11-20 20:37:50 +0300

    locale added. error handled removed

--- interactions/loader.py
+++ interactions/loader.py
@@ -1,21 +1,16 @@
 import logging
 
-from telegram import Update
-from telegram.ext import (
-    Application,
-    CallbackContext,
-    CommandHandler,
-    MessageHandler,
-    filters,
-)
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 from commands.details import details
 from commands.getgigs import getgigs
 from commands.nolastfm import nolastfm
 from commands.nonewevents import nonewevents
 from commands.start import start
+from interactions.common_handlers import error_handle
 from interactions.conn_lfm_conversation import conn_lfm_conversation
 from interactions.disconn_lfm_conversation import disconn_lfm_conversation
+from interactions.locale_conversation import locale_conversation
 
 logger = logging.getLogger('A.loa')
 logger.setLevel(logging.DEBUG)
@@ -25,6 +20,7 @@ def load_interactions(application: Application) -> None:
     """
     Loads all command handlers on start.
     Args: application: application for adding handlers to
+    TODO answer when unknown command sent
     """
     load_conversations(application)
     load_commands(application)
@@ -38,6 +34,7 @@ def load_conversations(application: Application) -> None:
     Loads all conversation handlers on start.
     Args: application: application for adding handlers to.
     """
+    application.add_handler(locale_conversation())
     application.add_handler(conn_lfm_conversation())
     application.add_handler(disconn_lfm_conversation())
     return None
@@ -69,13 +66,5 @@ def load_error(application: Application):
     Loads error handler message handlers on start.
     Args: application: application for adding handler to
     """
-
-    def error_handle(update: Update, context: CallbackContext) -> None:
-        """
-        Determine what to do when update causes error.
-        """
-        logger.warning(f'Update {update} caused error {context.error}')
-        return None
-
     application.add_error_handler(error_handle)
     return None
