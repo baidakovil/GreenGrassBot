fa55cf016b476696a099bf8ea894591871c118ec
baidakovil <baidakovil@gmail.com>
2023-11-20 20:42:24 +0300

    i34g

--- ui/error_builder.py
+++ ui/error_builder.py
@@ -1,7 +1,7 @@
-import i18n
+from services.message_service import i34g
 
 
-def error_text(error_code: int, acc: str) -> str:
+async def error_text(error_code: int, acc: str, user_id: int) -> str:
     """
     Returns error message for user.
     Args:
@@ -9,9 +9,11 @@ def error_text(error_code: int, acc: str) -> str:
         acc: account name raised the error, to make error more pleasible
     """
     error_dict = {
-        403: i18n.t("news_builders.403", acc=acc),
-        404: i18n.t("news_builders.404", acc=acc),
-        90: i18n.t("news_builders.90"),
+        403: await i34g("news_builders.403", acc=acc, user_id=user_id),
+        404: await i34g("news_builders.404", acc=acc, user_id=user_id),
+        90: await i34g("news_builders.90", user_id=user_id),
     }
-    some_error_text = i18n.t("news_builders.some_error", err=error_code, acc=acc)
+    some_error_text = await i34g(
+        "news_builders.some_error", err=error_code, acc=acc, user_id=user_id
+    )
     return error_dict.get(error_code, some_error_text)
