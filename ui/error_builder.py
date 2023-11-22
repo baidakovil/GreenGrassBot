d18896fec1b4ad52e7e57cb8326be7d6cef44e87
baidakovil <baidakovil@gmail.com>
2023-11-22 23:51:37 +0300

    error 91 non-http error added

--- ui/error_builder.py
+++ ui/error_builder.py
@@ -12,6 +12,7 @@ async def error_text(error_code: int, acc: str, user_id: int) -> str:
         403: await i34g("news_builders.403", acc=acc, user_id=user_id),
         404: await i34g("news_builders.404", acc=acc, user_id=user_id),
         90: await i34g("news_builders.90", user_id=user_id),
+        91: await i34g("news_builders.91", user_id=user_id),
     }
     some_error_text = await i34g(
         "news_builders.some_error", err=error_code, acc=acc, user_id=user_id
