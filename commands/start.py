50beddaf11e2c070d64acf6dea47d86ff943f81f
baidakovil <baidakovil@gmail.com>
2023-11-22 23:24:51 +0300

    typecheck: up and up_full func, developer alarming

--- commands/start.py
+++ commands/start.py
@@ -1,23 +1,27 @@
-from telegram import Update
+from telegram import Message, Update, User
 from telegram.ext import CallbackContext
 
+from config import Cfg
 from db.db import Db
-from services.message_service import i34g
-from services.message_service import alarm_char
-from services.message_service import reply
+from services.message_service import alarm_char, i34g, reply, send_message, up_full
 
 db = Db()
+CFG = Cfg()
 
 
 async def start(update: Update, context: CallbackContext) -> None:
     """
-    Callback function. Sends start message. If user have accounts, list it.
+    Callback function. Sends start message. Alarms developer. If user have accounts,
+    list it.
     Args:
         update, context: standart PTB callback signature
     """
+    user_id, _, _, username = up_full(update)
     await db.save_user(update)
-    user_id = update.message.from_user.id
-    username = update.message.from_user.first_name
+    if CFG.NEW_USER_ALARMING:
+        await send_message(
+            context, CFG.DEVELOPER_CHAT_ID, text=f'New user: {user_id}, {username}'
+        )
     lfm_accs = await db.rsql_lfmuser(user_id)
     if not lfm_accs:
         text = await i34g('start.user', username=username, user_id=user_id)
