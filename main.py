698434436240ad6ace5c0a703a68f52a9dbfa8de
baidakovil <baidakovil@gmail.com>
2023-11-29 13:34:46 +0300

    upd: timeouts, concurrency (need fixing), configs

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 from telegram.ext import Application
 
+from config import Cfg
 from db.db import Db
 from interactions.loader import load_interactions
 from services.logger import logger
 from services.schedule_service import reschedule_jobs
 
-i18n.load_path.append('./assets/lang')
-i18n.set('filename_format', '{locale}.{format}')
-i18n.set('locale', os.getenv("LANGUAGE"))
+CFG = Cfg()
+i18n.load_path.append(CFG.PATH_TRANSLATIONS)
+i18n.set('filename_format', CFG.FILENAME_FORMAT_I18N)
+i18n.set('locale', CFG.DEFAULT_LOCALE)
 
 logger = logging.getLogger('A.A')
 logger.setLevel(logging.DEBUG)
@@ -26,7 +28,14 @@ def main() -> None:
     """
     token = os.environ['BOT_TOKEN']
     db = Db(initial=True)
-    application = Application.builder().token(token).build()
+    application = (
+        Application.builder()
+        .token(token)
+        .read_timeout(CFG.SEC_READ_TIMEOUT)
+        .write_timeout(CFG.SEC_WRITE_TIMEOUT)
+        .concurrent_updates(True)
+        .build()
+    )
     load_interactions(application)
     reschedule_jobs(application, db)
     logger.info(f'App started')
