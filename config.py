d976a00d362fbd693e4848648275f2f313ac8529
baidakovil <baidakovil@gmail.com>
2023-12-05 22:56:26 +0300

    fix: full licence notices attached

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
-#  Green Grass Bot — A program to notify about concerts of artists you listened to.
-#  Copyright (C) 2021-2023 Ilia Baidakov <baidakovil@gmail.com>
-#  This program is free software: you can redistribute it and/or modify it under the
-#  terms of the GNU General Public License as published by the Free Software Foundation:
-#  GPLv3 or any later version at your option. License: <https://www.gnu.org/licenses/>.
+# Green Grass Bot — A program to notify about concerts of artists you listened to.
+# Copyright (C) 2021-2023 Ilia Baidakov <baidakovil@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <https://www.gnu.org/licenses/>.
 """This file contains Cfg class. Cfg and '.env' is the only configuration storages."""
 
 import logging
