e1a634217f79a57d7f575ed0fbf5a78afa9305f8
baidakovil <baidakovil@gmail.com>
2023-12-07 19:53:48 +0300

    upd: project renamed

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3
 
 # labels choosing just from Docker tutorial
 LABEL version="0.2"
-LABEL description="Telegram bot Great Gig Bot"
+LABEL description="Telegram bot Green Grass Bot"
 
 # set the working directory for COPY, RUN, CMD commands
 WORKDIR /usr/src/app
@@ -17,5 +17,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 # execute container
 CMD [ "python", "./main.py" ]
 
-# CLI run snippet V1: docker run --env-file <<path_to_'.env'_file>> baidakovil/greatgigbot
-# CLI run snippet V2: docker run --env API_KEY=<<lastfm_api_key>> --env BOT_TOKEN=<<telegram_bot_token>> baidakovil/greatgigbot
+# CLI run snippet V1: docker run --env-file <<path_to_'.env'_file>> baidakovil/greengrassbot:main
+# CLI run snippet V2: docker run --env API_KEY=<<lastfm_api_key>> --env BOT_TOKEN=<<telegram_bot_token>> baidakovil/greengrassbot:main
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-# checks if container name is supplied
-if [ "$#" -eq 0 ]
-then
-    echo "Please specify a container name!"
-    exit 1
-fi
+containername="greengrassbot"
+dockerhubrepo="baidakovil/greengrassbot:main"
 
 # checks if container exist
-if [ "$(docker ps -a -q -f name=$1)" ]
+if [ "$(docker ps --all --quiet --filter name=$containername)" ]
 then
-    echo "An existing container with the name $1 was found!"
+    echo "An existing container with the name $containername was found!"
     
     # checks if container is running and stop it if it is
-    if [ "$(docker ps -aq -f status=running -f name=$1)" ]
+    if [ "$(docker ps -aq -f status=running -f name=$containername)" ]
     then
-        echo "Stopping container..."
-        docker stop $1
+        echo "Stopping container $containername..."
+        docker stop $containername
 	echo "Container stopped."
     fi
 
     # removes stopped container
-    echo "Removing stopped container..."
-    docker rm $1
-    echo "Container removed."
+    echo "Removing stopped container $containername..."
+    docker rm $containername
+    echo "Container $containername removed."
 fi
 
 # pull the latest image
-docker pull baidakovil/greatgigbot:main
+docker pull $dockerhubrepo
 
-# run new docker container. be sure to run this file from subdirectory
-docker run -d --name $1 --env-file ../.env --restart=always baidakovil/greatgigbot:main
+# run new docker container. be sure to run this file from subdirectory of .env
+docker run -d --name $containername --env-file ../.env --restart=always $dockerhubrepo
+echo "Container $containername run."
