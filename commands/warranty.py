0dc9bcce2d6d0d00f028de4941c61d8e09f8aabf
baidakovil <baidakovil@gmail.com>
2023-12-12 00:08:54 +0300

    fix: db import deleted

--- commands/warranty.py
+++ commands/warranty.py
@@ -16,10 +16,7 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
-from db.db_service import Db
 from services.message_service import i34g, reply, up
-
-db = Db()
 
 
 async def warranty(update: Update, _context: CallbackContext) -> None:
